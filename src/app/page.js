d30724c437c26fc3ba0f422ddf5266f6bc6ecc1f
rycandole <113651785+rycandole@users.noreply.github.com>
2025-11-08 00:55:32 +0800

    update (#2)
    Co-authored-by: ryan candole <ry.candole@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,28 @@
-
+"use client"
 import React from 'react'
+import FlipBook from "@/Components/FlipBook"
+import styled from 'styled-components'
 
 const page = () => {
   return (
-    <div>page</div>
+    <MainWrapper >
+      <FlipBook />
+    </MainWrapper>
   )
 }
 
+const MainWrapper = styled.div`
+padding: 20px;
+
+
+.enveImg {
+  width: 100%
+}
+
+.mapsLinks {
+  display: flex
+}
+
+
+`
 export default page
