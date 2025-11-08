aff41a2c29f81da40edd041b62ae49defc897c7a
ryan candole <ry.candole@gmail.com>
2025-11-09 07:30:29 +0800

    update

--- src/Components/FlipBook.js
+++ src/Components/FlipBook.js
@@ -62,30 +62,39 @@ function Book() {
           <div className='rsvpPage'>
             <div className='rsvpWrap'>
               <Link className='rsvpLink ' target="_blank" rel="noopener noreferrer" href="https://forms.gle/rJ8zohpkrAjBQDo5A" >
-                <span className='rsvpTxt ms-madi'>Please RSVP Here</span>
+                <span className='rsvpTxt yellowTail'>Please RSVP here</span>
               </Link>
             </div></div>
           <div className='mapsLinks'>
-            <div>
-              <Link target="_blank" rel="noopener noreferrer" href="https://google.com/maps/place/Our+Lady+of+Mount+Carmel+Parish+-+Project+6+(Diocese+of+Cubao)/@14.6637505,121.0413698,17.25z/data=!4m6!3m5!1s0x3397b718ecd6d7c9:0x30b3057cbbbe0319!8m2!3d14.6624518!4d121.0414896!16s%2Fg%2F1tf6jr6g?entry=ttu&g_ep=EgoyMDI1MTEwNC4xIKXMDSoASAFQAw%3D%3D">
+            <div className='mapLinkWrap'>
+              <div className='mapLinkImg'>
                 <Image
+
                   src={church}
                   width={150}
                   height={150}
                   alt="envelope"
-                  className="Test"
+                  className="mapImg"
                 />
+              </div>
+              <Link className='mapBtnLink' target="_blank" rel="noopener noreferrer" href="https://google.com/maps/place/Our+Lady+of+Mount+Carmel+Parish+-+Project+6+(Diocese+of+Cubao)/@14.6637505,121.0413698,17.25z/data=!4m6!3m5!1s0x3397b718ecd6d7c9:0x30b3057cbbbe0319!8m2!3d14.6624518!4d121.0414896!16s%2Fg%2F1tf6jr6g?entry=ttu&g_ep=EgoyMDI1MTEwNC4xIKXMDSoASAFQAw%3D%3D">
+                <button className='mapBtn spectral'>Google map here</button>
+
               </Link>
             </div>
-            <div>
-              <Link target="_blank" rel="noopener noreferrer" href="https://google.com/maps/place/Ninoy+Aquino+Parks+and+Wildlife+Center/@14.6518021,121.044785,18.75z/data=!4m6!3m5!1s0x3397b7059eefe101:0x5271f95cbf47428a!8m2!3d14.6520871!4d121.0452732!16s%2Fm%2F05sxpms?entry=ttu&g_ep=EgoyMDI1MTEwNC4xIKXMDSoASAFQAw%3D%3D">
+            <div className='mapLinkWrap'>
+              <div className='mapLinkImg'>
                 <Image
                   src={wildlife}
                   width={150}
                   height={150}
                   alt="envelope"
-                  className="Test"
+                  className="mapImg"
                 />
+              </div>
+              <Link className='mapBtnLink' target="_blank" rel="noopener noreferrer" href="https://google.com/maps/place/Ninoy+Aquino+Parks+and+Wildlife+Center/@14.6518021,121.044785,18.75z/data=!4m6!3m5!1s0x3397b7059eefe101:0x5271f95cbf47428a!8m2!3d14.6520871!4d121.0452732!16s%2Fm%2F05sxpms?entry=ttu&g_ep=EgoyMDI1MTEwNC4xIKXMDSoASAFQAw%3D%3D">
+                <button className='mapBtn spectral'>Google map here</button>
+
               </Link>
             </div>
           </div>
