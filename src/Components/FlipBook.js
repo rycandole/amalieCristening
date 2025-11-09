dc7bfbe63ff3ab544fee9ccbb4e5a106b4227b19
rycandole <113651785+rycandole@users.noreply.github.com>
2025-11-09 08:45:03 +0800

    Fix (#6)
    * fix
    
    * update
    
    * fix
    
    ---------
    
    Co-authored-by: ryan candole <ry.candole@gmail.com>

--- src/Components/FlipBook.js
+++ src/Components/FlipBook.js
@@ -102,7 +102,7 @@ function Book() {
                   className="mapImg"
                 />
               </div>
-              <Link className='mapBtnLink' target="_blank" rel="noopener noreferrer" href="https://google.com/maps/place/Ninoy+Aquino+Parks+and+Wildlife+Center/@14.6518021,121.044785,18.75z/data=!4m6!3m5!1s0x3397b7059eefe101:0x5271f95cbf47428a!8m2!3d14.6520871!4d121.0452732!16s%2Fm%2F05sxpms?entry=ttu&g_ep=EgoyMDI1MTEwNC4xIKXMDSoASAFQAw%3D%3D">
+              <Link className='mapBtnLink' target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/BMB+Training+Center/@14.6520089,121.042314,17.25z/data=!4m14!1m7!3m6!1s0x3397b7059eefe101:0x5271f95cbf47428a!2sNinoy+Aquino+Parks+and+Wildlife+Center!8m2!3d14.6520871!4d121.0452732!16s%2Fm%2F05sxpms!3m5!1s0x3397b724344d2c11:0x3741eb207d250da1!8m2!3d14.6525804!4d121.0422841!16s%2Fg%2F11f6rb0fsq?entry=ttu&g_ep=EgoyMDI1MTEwNC4xIKXMDSoASAFQAw%3D%3D">
                 <button className='mapBtn spectral'>Google map here</button>
 
               </Link>
